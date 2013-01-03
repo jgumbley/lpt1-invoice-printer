cf32a39b1c2f949dd695c6045a4c7fd70bd3f325
Jim Gumbley <j.gumbley@gmail.com>
2013-01-03 13:52:52 +0000

    changes to support nicer feedback to user

--- process_invoices.py
+++ process_invoices.py
@@ -3,16 +3,19 @@ import os
 from time import sleep
 from reportlab.pdfgen import canvas
 import uuid
-
+import time
 
 LPT_OUTPUT_FILE = "C:\\mass-89\\invoice.txt"
+
+def log_to_screen(msg):
+    print time.strftime('%X %x') + " - " + msg
 
 def wait_for_change_to_file(filename):
     file_sig = file_signature(filename)
     while file_signature(filename) == file_sig:
         sleep(1)
-        print "waiting for an invoice."
-    print "found a change"
+        log_to_screen("waiting for an invoice.")
+    log_to_screen("found a change")
     return
 
 def file_signature(filename):
@@ -47,7 +50,7 @@ def blank_file(filename):
 
 
 def make_invoice(file_content, filename):
-    print "making a PDF"
+    log_to_screen("making a PDF")
     c = canvas.Canvas(filename)
     c.setFont("Courier", 11)
     x = 800
@@ -66,11 +69,20 @@ def make_pdf_filename():
     return str(uuid.uuid1())[8:] + ".pdf"
 
 def spawn_pdf_viewer(filename):
+    log_to_screen("opening PDF in viewer")
     os.system("start " + filename)
+
+def do_header(print_file):
+    hr = "**********************************************************"
+    log_to_screen(hr)
+    log_to_screen(" Watching for MASS89 Invoices via " + print_file)
+    log_to_screen(" - close this window when done printing.")
+    log_to_screen(hr)
 
 if __name__ == '__main__':
     while True:
         blank_file(LPT_OUTPUT_FILE)
+        do_header(LPT_OUTPUT_FILE)
         wait_for_change_to_file(LPT_OUTPUT_FILE)
         sleep(2) # make sure MASS89 has finished writing it
         file_content = dump_file(LPT_OUTPUT_FILE)

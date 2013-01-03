cecb477a16db566151ae7dac76141b3717f376a2
Jim Gumbley <j.gumbley@gmail.com>
2013-01-03 01:44:20 +0000

    using reportlab to whack out a PDF

--- process_invoices.py
+++ process_invoices.py
@@ -1,8 +1,9 @@
 import hashlib
 import os
 from time import sleep
+from reportlab.pdfgen import canvas
 
-LPT_OUTPUT_FILE = "invoice.txt"
+LPT_OUTPUT_FILE = "C:\\Users\\jgumbley\\Documents\\GitHub\\ascii-invoice-printer\\invoice.txt"
 
 def wait_for_change_to_file(filename):
     file_sig = file_signature(filename)
@@ -42,9 +43,30 @@ def dump_file(filename):
 def blank_file(filename):
     open(filename, 'w').close()
 
+
+def make_invoice(file_content):
+    print "making a PDF"
+    c = canvas.Canvas("hello.pdf")
+    c.setFont("Courier", 11)
+    x = 800
+    for line in file_content:
+        if "G" in line:
+            c.setFont("Courier-Bold", 11)
+        if "H" in line:
+            c.setFont("Courier", 11)
+        line = line.replace("G", "")
+        line = line.replace("H", "")
+        c.drawString(20,x,line[:-1])
+        x = x - 12
+    c.save()
+
+
 if __name__ == '__main__':
-    blank_file(LPT_OUTPUT_FILE)
-    wait_for_change_to_file(LPT_OUTPUT_FILE)
-    dump_file(LPT_OUTPUT_FILE)
-    blank_file(LPT_OUTPUT_FILE)
+    while True:
+        blank_file(LPT_OUTPUT_FILE)
+        wait_for_change_to_file(LPT_OUTPUT_FILE)
+        sleep(2) # make sure MASS89 has finished writing it
+        file_content = dump_file(LPT_OUTPUT_FILE)
+        make_invoice(file_content)
+        blank_file(LPT_OUTPUT_FILE)
 
